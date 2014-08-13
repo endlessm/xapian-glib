2820ce4633f679fde6705459a513aa160b7210a1
Will Greenberg <will@endlessm.com>
2014-08-12 19:14:52 -0700

    simple stopper first commit

---
+++ xapian-glib/xapian-simple-stopper.cc
@@ -0,0 +1,127 @@
+/* Copyright 2014  Endless Mobile
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * SECTION:xapian-simple-stopper
+ * @Title: XapianSimpleStopper
+ * @short_description: Simple stopper
+ */
+
+#include "config.h"
+
+#include "xapian-simple-stopper.h"
+#include "xapian-error-private.h"
+
+#define XAPIAN_SIMPLE_STOPPER_GET_PRIVATE(obj) \
+  ((XapianSimpleStopperPrivate *) xapian_simple_stopper_get_instance_private ((XapianSimpleStopper *) (obj)))
+
+typedef struct {
+  Xapian::SimpleStopper *mSimpleStopper;
+} XapianSimpleStopperPrivate;
+
+static void initable_iface_init (GInitableIface *iface);
+
+G_DEFINE_TYPE_WITH_CODE (XapianSimpleStopper, xapian_simple_stopper, G_TYPE_OBJECT,
+                         G_ADD_PRIVATE (XapianSimpleStopper)
+                         G_IMPLEMENT_INTERFACE (G_TYPE_INITABLE, initable_iface_init))
+
+static gboolean
+xapian_simple_stopper_init_internal (GInitable    *self,
+                              GCancellable *cancellable,
+                              GError      **error)
+{
+}
+
+static void
+initable_iface_init (GInitableIface *iface)
+{
+  iface->init = xapian_simple_stopper_init_internal;
+}
+
+static void
+xapian_simple_stopper_finalize (GObject *gobject)
+{
+  XapianSimpleStopperPrivate *priv = XAPIAN_SIMPLE_STOPPER_GET_PRIVATE (gobject);
+
+  delete priv->mSimpleStopper;
+
+  G_OBJECT_CLASS (xapian_simple_stopper_parent_class)->finalize (gobject);
+}
+
+static void
+xapian_simple_stopper_dispose (GObject *gobject)
+{
+  XapianSimpleStopperPrivate *priv = XAPIAN_SIMPLE_STOPPER_GET_PRIVATE (gobject);
+
+  G_OBJECT_CLASS (xapian_simple_stopper_parent_class)->dispose (gobject);
+}
+
+static void
+xapian_simple_stopper_class_init (XapianSimpleStopperClass *klass)
+{
+  GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
+
+  gobject_class->dispose = xapian_simple_stopper_dispose;
+  gobject_class->finalize = xapian_simple_stopper_finalize;
+}
+
+static void
+xapian_simple_stopper_init (XapianSimpleStopper *stopper)
+{
+  XapianSimpleStopperPrivate *priv = XAPIAN_SIMPLE_STOPPER_GET_PRIVATE (stopper);
+
+  priv->mSimpleStopper = new Xapian::SimpleStopper ();
+}
+
+void
+xapian_simple_stopper_add (XapianSimpleStopper *stopper,
+                           const gchar *word)
+{
+  g_return_if_fail (XAPIAN_IS_SIMPLE_STOPPER (stopper));
+
+  XapianSimpleStopperPrivate *priv = XAPIAN_SIMPLE_STOPPER_GET_PRIVATE (stopper);
+
+  priv->mSimpleStopper->add(word);
+}
+
+char *
+xapian_simple_stopper_get_description (XapianSimpleStopper *stopper)
+{
+  g_return_val_if_fail (XAPIAN_IS_SIMPLE_STOPPER (stopper), NULL);
+
+  XapianSimpleStopperPrivate *priv = XAPIAN_SIMPLE_STOPPER_GET_PRIVATE (stopper);
+
+  std::string desc = priv->mSimpleStopper->get_description ();
+
+  return g_strdup (desc.c_str ());
+}
+
+/**
+ * xapian_simple_stopper_new:
+ * @error: return location for a #GError, or %NULL
+ *
+ * If the initializion failed, @error is set, and this function
+ * will return %NULL.
+ *
+ * Returns: (transfer full): the newly created #XapianSimpleStopper instance
+ */
+XapianSimpleStopper *
+xapian_simple_stopper_new (GError        **error)
+{
+  return static_cast<XapianSimpleStopper *> (g_initable_new (XAPIAN_TYPE_SIMPLE_STOPPER,
+                                                       NULL, error,
+                                                       NULL));
+}
---
+++ xapian-glib/xapian-simple-stopper.h
@@ -0,0 +1,61 @@
+/* Copyright 2014  Endless Mobile
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __XAPIAN_GLIB_SIMPLE_STOPPER_H__
+#define __XAPIAN_GLIB_SIMPLE_STOPPER_H__
+
+#if !defined(XAPIAN_GLIB_H_INSIDE) && !defined(XAPIAN_GLIB_COMPILATION)
+#error "Only <xapian-glib.h> can be included directly."
+#endif
+
+#include "xapian-glib-types.h"
+
+G_BEGIN_DECLS
+
+#define XAPIAN_SIMPLE_STOPPER(obj)             (G_TYPE_CHECK_INSTANCE_CAST ((obj), XAPIAN_TYPE_SIMPLE_STOPPER, XapianSimpleStopper))
+#define XAPIAN_IS_SIMPLE_STOPPER(obj)          (G_TYPE_CHECK_INSTANCE_TYPE ((obj), XAPIAN_TYPE_SIMPLE_STOPPER))
+#define XAPIAN_SIMPLE_STOPPER_CLASS(klass)     (G_TYPE_CHECK_CLASS_CAST ((klass), XAPIAN_TYPE_SIMPLE_STOPPER, XapianSimpleStopperClass))
+#define XAPIAN_IS_SIMPLE_STOPPER_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE ((klass), XAPIAN_TYPE_SIMPLE_STOPPER))
+#define XAPIAN_SIMPLE_STOPPER_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS ((obj), XAPIAN_TYPE_SIMPLE_STOPPER, XapianSimpleStopperClass))
+
+typedef struct _XapianSimpleStopperClass      XapianSimpleStopperClass;
+
+struct _XapianSimpleStopper
+{
+  GObject parent_instance;
+};
+
+struct _XapianSimpleStopperClass
+{
+  GObjectClass parent_instance;
+};
+
+XAPIAN_GLIB_AVAILABLE_IN_1_0
+GType xapian_simple_stopper_get_type (void);
+
+XAPIAN_GLIB_AVAILABLE_IN_1_0
+XapianSimpleStopper * xapian_simple_stopper_new (GError **error);
+
+XAPIAN_GLIB_AVAILABLE_IN_1_0
+void xapian_simple_stopper_add (XapianSimpleStopper *stopper,
+                                const gchar *word);
+
+XAPIAN_GLIB_AVAILABLE_IN_1_0
+char * xapian_simple_stopper_get_description (XapianSimpleStopper *stopper);
+
+G_END_DECLS
+
+#endif /* __XAPIAN_GLIB_SIMPLE_STOPPER_H__ */
