6bad0353efd9ac3ab4a73a6c36f8e338f7cb296f
Olly Betts <olly@survex.com>
2017-05-10 13:29:45 +1200

    Minor indentation fix

--- xapian-glib/xapian-query-parser.h
+++ xapian-glib/xapian-query-parser.h
@@ -57,7 +57,7 @@ void                    xapian_query_parser_set_stemming_strategy       (XapianQ
                                                                          XapianStemStrategy        strategy);
 XAPIAN_GLIB_AVAILABLE_IN_1_2
 void                    xapian_query_parser_set_stopper                 (XapianQueryParser        *parser,
-                                                                         XapianStopper               *stopper);
+                                                                         XapianStopper            *stopper);
 XAPIAN_GLIB_AVAILABLE_IN_1_0
 void                    xapian_query_parser_set_database                (XapianQueryParser        *parser,
                                                                          XapianDatabase           *database);
