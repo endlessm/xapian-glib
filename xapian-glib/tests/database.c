3f69352337e5a1729175766ba69eb3c9f2116157
Emmanuele Bassi <ebassi@gnome.org>
2017-05-26 20:54:18 +0100

    tests: Add a writable database test
    Ensure that we can open a writable database.

--- xapian-glib/tests/database.c
+++ xapian-glib/tests/database.c
@@ -1,4 +1,5 @@
 #include <glib.h>
+#include <glib/gstdio.h>
 #include "xapian-glib.h"
 
 static void
@@ -31,6 +32,49 @@ database_new_nonexistent (void)
   g_error_free (error);
 }
 
+static void
+database_writable_new (void)
+{
+  GError *error = NULL;
+  XapianWritableDatabase *db = xapian_writable_database_new ("doesexist",
+                                                             XAPIAN_DATABASE_ACTION_CREATE_OR_OPEN,
+                                                             &error);
+
+  g_assert_nonnull (db);
+
+  g_object_add_weak_pointer (G_OBJECT (db), (gpointer *) &db);
+
+  g_assert_true (XAPIAN_IS_WRITABLE_DATABASE (db));
+  g_assert_true (XAPIAN_IS_DATABASE (db));
+
+  g_object_unref (db);
+  g_assert_null (db);
+
+  g_assert_true (g_file_test ("doesexist", G_FILE_TEST_EXISTS | G_FILE_TEST_IS_DIR));
+
+  {
+    GDir *d = g_dir_open ("doesexist", 0, NULL);
+    const char *name;
+
+    while ((name = g_dir_read_name (d)) != NULL)
+      {
+        char *path;
+
+        if ((name[0] == '.' && name[1] == '\0') ||
+            (name[0] == '.' && name[1] == '.' && name[2] == '\0'))
+          continue;
+
+        path = g_build_filename ("doesexist", name, NULL);
+        g_unlink (path);
+        g_free (path);
+      }
+
+    g_dir_close (d);
+
+    g_rmdir ("doesexist");
+  }
+}
+
 int
 main (int argc, char *argv[])
 {
@@ -38,6 +82,7 @@ main (int argc, char *argv[])
 
   g_test_add_func ("/database/new/empty", database_new_empty);
   g_test_add_func ("/database/new/non-existent", database_new_nonexistent);
+  g_test_add_func ("/database/writable/new", database_writable_new);
 
   return g_test_run ();
 }
