edee29d4f774a7db13225ea109a916e8bb46d63b
Emmanuele Bassi <ebassi@gnome.org>
2017-05-27 11:24:03 +0100

    Add constructor tests for WritableDatabase
    Check that we can create glass databases, and that action, backend, and
    flags values are honored.

--- xapian-glib/tests/database.c
+++ xapian-glib/tests/database.c
@@ -36,9 +36,10 @@ static void
 database_writable_new (void)
 {
   GError *error = NULL;
-  XapianWritableDatabase *db = xapian_writable_database_new ("doesexist",
-                                                             XAPIAN_DATABASE_ACTION_CREATE_OR_OPEN,
-                                                             &error);
+  XapianWritableDatabase *db =
+    xapian_writable_database_new ("doesexist",
+                                  XAPIAN_DATABASE_ACTION_CREATE,
+                                  &error);
 
   g_assert_nonnull (db);
 
@@ -75,6 +76,99 @@ database_writable_new (void)
   }
 }
 
+static void
+database_writable_backend_glass (void)
+{
+  GError *error = NULL;
+  XapianWritableDatabase *db =
+    xapian_writable_database_new_with_backend ("glass-db",
+                                               XAPIAN_DATABASE_ACTION_CREATE,
+                                               XAPIAN_DATABASE_BACKEND_GLASS,
+                                               &error);
+
+  g_assert_nonnull (db);
+
+  g_object_add_weak_pointer (G_OBJECT (db), (gpointer *) &db);
+
+  g_assert_true (XAPIAN_IS_WRITABLE_DATABASE (db));
+  g_assert_true (XAPIAN_IS_DATABASE (db));
+
+  g_object_unref (db);
+  g_assert_null (db);
+
+  g_assert_true (g_file_test ("glass-db", G_FILE_TEST_EXISTS | G_FILE_TEST_IS_DIR));
+  g_assert_true (g_file_test ("glass-db/termlist.glass", G_FILE_TEST_EXISTS));
+
+  {
+    GDir *d = g_dir_open ("glass-db", 0, NULL);
+    const char *name;
+
+    while ((name = g_dir_read_name (d)) != NULL)
+      {
+        char *path;
+
+        if ((name[0] == '.' && name[1] == '\0') ||
+            (name[0] == '.' && name[1] == '.' && name[2] == '\0'))
+          continue;
+
+        path = g_build_filename ("glass-db", name, NULL);
+        g_unlink (path);
+        g_free (path);
+      }
+
+    g_dir_close (d);
+
+    g_rmdir ("glass-db");
+  }
+}
+
+static void
+database_writable_flags_no_termlist (void)
+{
+  GError *error = NULL;
+  XapianWritableDatabase *db =
+    xapian_writable_database_new_full ("glass-db",
+                                       XAPIAN_DATABASE_ACTION_CREATE,
+                                       XAPIAN_DATABASE_BACKEND_GLASS,
+                                       XAPIAN_DATABASE_FLAGS_NO_TERMLIST,
+                                       &error);
+
+  g_assert_nonnull (db);
+
+  g_object_add_weak_pointer (G_OBJECT (db), (gpointer *) &db);
+
+  g_assert_true (XAPIAN_IS_WRITABLE_DATABASE (db));
+  g_assert_true (XAPIAN_IS_DATABASE (db));
+
+  g_object_unref (db);
+  g_assert_null (db);
+
+  g_assert_true (g_file_test ("glass-db", G_FILE_TEST_EXISTS | G_FILE_TEST_IS_DIR));
+  g_assert_false (g_file_test ("glass-db/termlist.glass", G_FILE_TEST_EXISTS));
+
+  {
+    GDir *d = g_dir_open ("glass-db", 0, NULL);
+    const char *name;
+
+    while ((name = g_dir_read_name (d)) != NULL)
+      {
+        char *path;
+
+        if ((name[0] == '.' && name[1] == '\0') ||
+            (name[0] == '.' && name[1] == '.' && name[2] == '\0'))
+          continue;
+
+        path = g_build_filename ("glass-db", name, NULL);
+        g_unlink (path);
+        g_free (path);
+      }
+
+    g_dir_close (d);
+
+    g_rmdir ("glass-db");
+  }
+}
+
 int
 main (int argc, char *argv[])
 {
@@ -83,6 +177,8 @@ main (int argc, char *argv[])
   g_test_add_func ("/database/new/empty", database_new_empty);
   g_test_add_func ("/database/new/non-existent", database_new_nonexistent);
   g_test_add_func ("/database/writable/new", database_writable_new);
+  g_test_add_func ("/database/writable/backend/glass", database_writable_backend_glass);
+  g_test_add_func ("/database/writable/flags/no-termlist", database_writable_flags_no_termlist);
 
   return g_test_run ();
 }
