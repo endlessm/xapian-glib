780a38449d19e3333c3aee0a250eea1650453608
Emmanuele Bassi <ebassi@gnome.org>
2017-05-30 18:46:19 +0100

    Fix the internal Xapian version check
    The existing check is backwards, but it's also very subtle to get wrong.
    
    We can simplify it, and immediately get if it's wrong, by encoding the
    versions into a single number.

--- xapian-glib/xapian-private.h
+++ xapian-glib/xapian-private.h
@@ -1,10 +1,16 @@
 #ifndef __XAPIAN_PRIVATE_H__
 #define __XAPIAN_PRIVATE_H__
 
+#include <xapian.h>
+
+#define _XAPIAN_VERSION_ENCODE_INTERNAL(maj,min,rev) \
+  ((maj * 10000) + (min * 100) + rev)
+
+#define _XAPIAN_CUR_VERSION \
+  _XAPIAN_VERSION_ENCODE_INTERNAL(XAPIAN_MAJOR_VERSION,XAPIAN_MINOR_VERSION,XAPIAN_REVISION)
+
 /* This is an internal version check macro for the Xapian version */
 #define XAPIAN_CHECK_VERSION_INTERNAL(maj,min,rev) \
-  ((maj) > XAPIAN_MAJOR_VERSION || \
-   ((maj) == XAPIAN_MAJOR_VERSION && (min) > XAPIAN_MINOR_VERSION) || \
-   ((maj) == XAPIAN_MAJOR_VERSION && (min) == XAPIAN_MINOR_VERSION && (rev) >= XAPIAN_REVISION))
+   (_XAPIAN_CUR_VERSION > _XAPIAN_VERSION_ENCODE_INTERNAL(maj,min,rev))
 
 #endif
