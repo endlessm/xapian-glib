c8a564d769e554451b05de851232d635b26fd713
Olly Betts <olly@survex.com>
2017-07-18 17:21:18 +1200

    Add/update copyright headers

--- xapian-glib/xapian-term-iterator-private.h
+++ xapian-glib/xapian-term-iterator-private.h
@@ -1,4 +1,4 @@
-/* Copyright 2014  Endless Mobile
+/* Copyright 2017  Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
